4b1ad7373d4167a2d6ee9e0c28c6c5911a31b2b3
jmclain <jmclain3@asu.edu>
2023-12-08 13:20:55 -0700

    init commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { loadScript } from '@paypal/paypal-js';
+let paypal;
+try {
+  paypal = await loadScript({ clientId: process.env.PAYPAL_CLIENT_ID as string });
+} catch (error) {
+  console.error(error);
+}
+if (paypal) {
+  try {
+    await paypal.Buttons().render('#paypal-buttons');
+  } catch (error) {
+    console.error('failed to load buttons', error);
+  }
+}
+</script>
+<template>
+  <div class="flex justify-center items-center h-[100]" >
+    <div id="paypal-buttons"></div>
+  </div>
+</template>
---
+++ util/helpers.ts
@@ -0,0 +1,5 @@
+function getPaypalCredentials() {
+    const client_id = process.env.PAYPAL_CLIENT_ID
+    const secert = process.env.PAYPAL_SECRET
+
+}
